83de44e6b36826cb94a228dcb81a2a9cd1ce8ae9
AlenaPanPanda <guiyguker@gmail.com>
2025-05-06 12:48:04 +0200

    nav bar

--- feature/main_screen/impl/src/main/java/recipia/feature/main_screen/impl/BottomNavBar.kt
+++ feature/main_screen/impl/src/main/java/recipia/feature/main_screen/impl/BottomNavBar.kt
@@ -1,15 +1,20 @@
 package recipia.feature.main_screen.impl
 
-import androidx.compose.foundation.Image
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import recipia.core.ui.icons.Icons
 import recipia.core.ui.theme.basil
-import recipia.core.ui.theme.creamWhite
+import recipia.core.ui.theme.freshGreen
+import recipia.core.ui.theme.snowWhite
 
 @Composable
 fun BottomNavBar(
@@ -18,24 +23,28 @@ fun BottomNavBar(
 ) {
     val navigationItems = listOf(
         NavigationItem("Recipes", icon = Icons.collections1, MenuItem.RecipeList),
+        NavigationItem("Add recipe", icon = Icons.addRecipe, MenuItem.AddRecipe),
+        NavigationItem("Recipes", icon = Icons.collections1, MenuItem.RecipeList),
         NavigationItem("Add recipe", icon = Icons.addRecipe, MenuItem.AddRecipe)
     )
 
-    NavigationBar(containerColor = creamWhite) {
+    NavigationBar(containerColor = snowWhite) {
         navigationItems.forEachIndexed { index, item ->
             NavigationBarItem(
                 selected = selectedIndex == index,
                 onClick = { onItemSelected(index, item.screen) },
                 icon = {
-                    Image(
-                        painter = painterResource(item.icon),
-                        contentDescription = item.title
+                    Icon(
+                        imageVector = ImageVector.vectorResource(id = item.icon),
+                        contentDescription = item.title,
+                        modifier = Modifier.size(28.dp),
                     )
                 },
                 colors = NavigationBarItemDefaults.colors(
-                    selectedIconColor = basil,
-                    indicatorColor = basil
-                )
+                    selectedIconColor = snowWhite,
+                    unselectedIconColor = basil,
+                    indicatorColor = freshGreen
+                ),
             )
         }
     }
