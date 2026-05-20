e5cb452db967b5e221e3d2c2e44303b1fd9a7720
aharavaya <guiyguker@gmail.com>
2026-05-20 22:00:03 +0200

    test git

--- feature/addrecipe/impl/src/main/java/recipia/feature/add_recipe/impl/domain/mapper/RecipeMapper.kt
+++ feature/addrecipe/impl/src/main/java/recipia/feature/add_recipe/impl/domain/mapper/RecipeMapper.kt
@@ -2,7 +2,7 @@ package recipia.feature.add_recipe.impl.domain.mapper
 
 import com.example.recipia.core.common.model.FullRecipe
 import recipia.feature.add_recipe.impl.data.dto.GetRecipeResponse
-
+//mapper
 internal interface RecipeMapper {
     fun convert(response: GetRecipeResponse): FullRecipe
 }
